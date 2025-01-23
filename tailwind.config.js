30df4465d040fce9e8e7370d15af5bac68e4c528
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2025-01-23 09:17:43 +0600

    Build navbar and banner sections with responsive design

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: [
+      './src/**/*.{html,js}'
+    ],
+    theme: {
+      extend: {
+        screens: {
+          'phone': '640px',
+          'tablet': '768px',
+          'laptop': '1024px',
+          'desktop': '1280px',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
