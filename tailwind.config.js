f8bc979186203f3179967bc2683bab6598ed6fcc
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2025-01-23 23:06:22 +0600

    added impressive stats section with tailwind css - 500k+ users, 1.7L tickets sold, 80k+ partners

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ module.exports = {
     ],
     theme: {
       extend: {
-        screens: {
-          'phone': '640px',
-          'tablet': '768px',
-          'laptop': '1024px',
-          'desktop': '1280px',
-        },
+        // screens: {
+        //   'phone': '640px',
+        //   'tablet': '768px',
+        //   'laptop': '1024px',
+        //   'desktop': '1280px',
+        // },
       },
     },
     plugins: [],
