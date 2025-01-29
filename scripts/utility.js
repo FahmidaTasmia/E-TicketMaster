91f5d1aee1881a5ffa98d003183c23f80c02d1c5
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2025-01-30 00:15:41 +0600

    Implement coupon validation with discounts

--- scripts/utility.js
+++ scripts/utility.js
@@ -12,7 +12,29 @@ return couponCode;
 };
 
 //Helper function to Validate coupon code 
-export const ValidateCoupon =(couponCode)=>{
+const ValidateCoupon =(couponCode)=>{
     const ValidCoupons = ["NEW15","Couple20"];
     return ValidCoupons.includes(couponCode);
 };
+
+// calculate  total Price With Discounts
+const calculateTotal = (selectedSeats, couponCode)=>{
+    let discount = 0;
+    const basePrice = selectedSeats.length * 550;
+    
+    //Automatic 20% discount for 4 tickets
+    if(selectedSeats.length === 4){
+        discount = basePrice * 0.2; //20% discount
+    }
+
+    //Apply coupon discount if Valid
+    if(couponCode === "NEW15"){
+        discount += basePrice * 0.15; //15%discount
+    }
+    else if(couponCode === "Couple20"){
+        discount += basePrice * 0.2; //20%discount
+    }
+
+    return basePrice - discount;
+
+}
